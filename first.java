d4f80758916d7f0258eaa9aae6bae927cf8bdc8f
= <=>
2018-01-08 18:33:36 +0530

    ehehehe hiii

---
+++ first.java
@@ -0,0 +1,19 @@
+class first
+{
+public static void main(String[] args)
+{
+int i=2;
+switch(i)
+{
+case 0:
+System.out.println("i=0");
+break;
+case 1:
+System.out.println("i=1");
+break;
+case 2:
+System.out.println("i=2");
+break;
+}
+}
+}
